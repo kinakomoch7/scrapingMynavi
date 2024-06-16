9845a5fc993d82ea4414ec0c8cd195a558c2658f
kinakomoch7 <book-insect14@outlook.jp>
2024-06-17 00:04:39 +0900

    [fix]すでに存在する場合には登録しない

--- components/addData.js
+++ components/addData.js
@@ -1,12 +1,23 @@
 const sql = require("./neonDB.js");
 
-module.exports = async function addData(props) {
+const addData = async (props) => {
   const { name, deadline, industry } = props;
 
   try {
+    const res = await sql`SELECT * FROM job_listings WHERE name = ${name}`;
+
+    // すでにデータが存在する場合は追加しない
+    if (res.length > 0) {
+      console.log(`already exists`);
+      return;
+    }
+
+    // データを追加
     await sql`INSERT INTO job_listings (name, deadline, industry ) VALUES (${name}, ${deadline}, ${industry})`;
     console.log(`success`);
   } catch (e) {
-    throw new Error(e);
+    console.error(e);
   }
 };
+
+module.exports = addData;
